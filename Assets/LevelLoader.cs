3989d65d2e2573e03af896a71b773073ae17091b
Marc Taurasi <altairre.atsu@gmail.com>
2018-04-12 00:24:37 -0600

    Initial Commit

---
+++ Assets/LevelLoader.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Invoke("LoadScene", 1f);
+	}
+	
+	// Update is called once per frame
+	void LoadScene() {
+		Scoreboard.runningScore += 10;
+		SceneManager.LoadScene(1);
+	}
+}
---
+++ Assets/Scoreboard.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scoreboard : MonoBehaviour {
+
+	public static int runningScore = 10;
+
+	void Start () {
+		print(runningScore);
+	}
+}
